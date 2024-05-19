ad9a7bef56b9851a365506531277b231ea232e95
hidelrios <hidels.rios@gmail.com>
2024-05-18 22:22:44 -0300

    implementado relatorio pdf

--- sfcafe/core/forms.py
+++ sfcafe/core/forms.py
@@ -4,7 +4,6 @@ from .models import Cliente, Ingrediente, ItemCardapio, ItemPedido, Pedido
 from django.forms import inlineformset_factory
 
 
-
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
@@ -23,16 +22,21 @@ class ItemPedidoForm(forms.ModelForm):
         model = ItemPedido
         fields = ['item_cardapio', 'quantidade', 'observacoes']
 
+
 class PedidoForm(forms.ModelForm):
-    itens = forms.inlineformset_factory(Pedido, ItemPedido, form=ItemPedidoForm, extra=1)
+    itens = forms.inlineformset_factory(
+        Pedido, ItemPedido, form=ItemPedidoForm, extra=1)
 
     class Meta:
         model = Pedido
-        fields = ['cliente','status','metodo_pagamento']
+        fields = ['cliente', 'status', 'metodo_pagamento']
 
-ItemPedidoFormSet = inlineformset_factory(Pedido, ItemPedido, form=ItemPedidoForm, extra=1, can_delete=True)
+
+ItemPedidoFormSet = inlineformset_factory(
+    Pedido, ItemPedido, form=ItemPedidoForm, extra=1, can_delete=True)
+
 
 class IngredienteForm(forms.ModelForm):
     class Meta:
-        model= Ingrediente
+        model = Ingrediente
         fields = "__all__"
--- sfcafe/core/signals.py
+++ sfcafe/core/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import ItemPedido, Ingrediente, Produto
 
+
 @receiver(post_save, sender=ItemPedido)
 def atualizar_estoque_apos_adicionar(sender, instance, **kwargs):
     for ingrediente in instance.item_cardapio.ingredientes.all():
@@ -10,6 +11,7 @@ def atualizar_estoque_apos_adicionar(sender, instance, **kwargs):
     for produto in instance.item_cardapio.produtos.all():
         produto.quantidade_estoque -= instance.quantidade
         produto.save()
+
 
 @receiver(post_delete, sender=ItemPedido)
 def atualizar_estoque_apos_remover(sender, instance, **kwargs):
